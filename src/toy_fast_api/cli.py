3bcc1da4d8d4662552cdbde4204259ac81b67e85
root <shreelakshmi.iyengar@diamond.ac.uk>
2026-04-07 09:39:20 +0000

    feat: add version callback to cli.py

--- src/toy_fast_api/cli.py
+++ src/toy_fast_api/cli.py
@@ -1,8 +1,27 @@
+from typing import Annotated
+
 import typer
 
 from toy_fast_api.main import odd_or_even, start
 
+from . import __version__
+
 cli_app = typer.Typer()
+
+
+def version_callback(value: bool):
+    if value:
+        print(f"Version: {__version__}")
+        raise typer.Exit()
+
+
+@cli_app.callback()
+def callback(
+    version: Annotated[
+        bool | None, typer.Option("--version", callback=version_callback)
+    ] = None,
+):
+    pass
 
 
 @cli_app.command()
