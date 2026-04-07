e2dc004eeed6a3d514b228f3c916252f5fe2b14b
root <shreelakshmi.iyengar@diamond.ac.uk>
2026-04-07 09:42:18 +0000

    tests: add test for version callbacl

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,15 @@
 from typer.testing import CliRunner
 
+from toy_fast_api import __version__
 from toy_fast_api.cli import cli_app
 
 runner = CliRunner()
+
+
+def test_callback():
+    result = runner.invoke(cli_app, ["--version"])
+    assert result.exit_code == 0
+    assert result.stdout.strip() == f"Version: {__version__}"
 
 
 def test_check_even():
